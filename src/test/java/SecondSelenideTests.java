a1d934b599cedbea92bb2b6a8d69f56dd5f3f5b4
Ekaterina Goncharova <katinagon@yandex.ru>
2025-04-16 18:53:40 +0300

    Added class with 2 tests (load the page, drag and drop)

---
+++ src/test/java/SecondSelenideTests.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
+
+public class SecondSelenideTests {
+    @Test
+    @DisplayName("Загрузка страницы Enterprize")
+    void loadTheEnterprizePageTest() {
+        open("https://github.com/");
+        $(byTagAndText("button","Solutions")).hover();
+        $(byTagAndText("a","Enterprises")).click();
+        $("h1[id=hero-section-brand-heading]").shouldHave(text("The AI-powered\n" +
+                "developer platform"));
+    }
+
+    @Test
+    @DisplayName("Drag And Drop")
+    void programDragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a header").shouldHave(text("B"));
+        $("#column-b header").shouldHave(text("A"));
+    }
+}
