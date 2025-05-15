7bff20f1ba2696f30a03cb01c55f5139f155b553
Ekaterina Goncharova <katinagon@yandex.ru>
2025-05-15 10:28:45 +0300

    Fixed video

--- src/test/java/tests/TestBaseDemoQA.java
+++ src/test/java/tests/TestBaseDemoQA.java
@@ -2,10 +2,19 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBaseDemoQA {
     @BeforeAll
     public static void beforeAll() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
