7b0e254e69a56c3fff190af5ed0ccaa063d1b2d2
Ekaterina Goncharova <katinagon@yandex.ru>
2025-05-20 12:03:16 +0300

    Added sout with browser

--- src/test/java/properties/SystemPropertiesTest.java
+++ src/test/java/properties/SystemPropertiesTest.java
@@ -8,17 +8,20 @@ public class SystemPropertiesTest {
     @Tag("chrome")
     void testInChrome() {
         String browser = System.getProperty("browser", "chrome");
+        System.out.println(browser);
     }
 
     @Test
     @Tag("firefox")
     void testInFirefox() {
         String browser = System.getProperty("browser", "firefox");
+        System.out.println(browser);
     }
 
     @Test
     @Tag("opera")
     void testInOpera() {
         String browser = System.getProperty("browser", "opera");
+        System.out.println(browser);
     }
 }
